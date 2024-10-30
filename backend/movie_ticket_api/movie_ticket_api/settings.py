196c2b4a6ad4a2dfd9604af8c2470fbf8a9b3a0b
Henry <henry.a.s.arrovin@gmail.com>
2024-10-30 18:54:38 +0530

    Updated jwt lifetime.

--- backend/movie_ticket_api/movie_ticket_api/settings.py
+++ backend/movie_ticket_api/movie_ticket_api/settings.py
@@ -96,8 +96,11 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
 
